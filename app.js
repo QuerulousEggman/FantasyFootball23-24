58898052c34ce14115ae202bbb8623cb9021935a
QuerulousEggman <ultimofantasista10@gmail.com>
2023-08-31 22:25:02 +0100

    updated 310823

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var teamObject = {
+const teamObject = {
   "Arsenal": {
     "Goalkeeper": ["Aaron Ramsdale", "David Raya"],
     "Defender": ["Ben White", "Takehiro Tomiyasu", "Gabriel", "William Saliba", "Oleksandr Zinchenko", "Jurrien Timber", "Jakub Kiwior", "Kieran Tierney", "Rob Holding", "Cedric Soares"],
@@ -13,36 +13,42 @@ var teamObject = {
   }
 }
 window.onload = function() {
-  var teamSel = document.getElementById("teams");
-  var positionSel = document.getElementById("position");
-  var playerSel = document.getElementById("player")
-  for (var x in teamObject) {
+  const teamSel = document.getElementById("teams");
+  const positionSel = document.getElementById("position");
+  const playerSel = document.getElementById("player")
+  for (const x in teamObject) {
     teamSel.options[teamSel.options.length] = new Option(x, x);
   }
   teamSel.onchange = function() {
-    //empty Chapters- and Topics- dropdowns
+    //empty Position and Player- dropdowns
     positionSel.length = 1;
     //display correct values
-    for (var y in teamObject[this.value]) {
+    for (const y in teamObject[this.value]) {
       positionSel.options[positionSel.options.length] = new Option(y, y);
     }
   }
   positionSel.onchange = function() {
-    //empty Chapters dropdown
+    //empty Player dropdown
     playerSel.length = 1;
     //display correct values
-    var z = teamObject[teamSel.value][this.value];
-    for (var i = 0; i < z.length; i++) {
+    let z = teamObject[teamSel.value][this.value];
+    for (let i = 0; i < z.length; i++) {
       playerSel.options[playerSel.options.length] = new Option(z[i], z[i]);
     }
   }
 }
 
-const navExpandButton = document.querySelector('#nav-expand-button')
-const nav = document.getElementById('mainNav')
-function handleClick() {
-  nav.classList.toggle('open')
-  const expandedState = nav.getAttribute('aria-expanded')
-  nav.setAttribute('aria-expanded', !eval(expandedState))
-};
-navExpandButton.addEventListener('click', handleClick)
+// const navExpandButton = document.querySelector('#nav-expand-button')
+// const nav = document.getElementById('mainNav')
+// function handleClick() {
+//   nav.classList.toggle('open')
+//   const expandedState = nav.getAttribute('aria-expanded')
+//   nav.setAttribute('aria-expanded', !eval(expandedState))
+// };
+// navExpandButton.addEventListener('click', handleClick)
+
+function showInput() {
+  document.getElementById("position").value;
+  document.getElementById("player").value;
+  document.getElementById("teams").value;
+}
